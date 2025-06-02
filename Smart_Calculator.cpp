569127c76085e2cbb53f319d1bce3d83f5a431a8
Leon Siew <1211206814@student.mmu.edu.my>
2025-06-02 21:54:36 +0800

    Average daily electric usage

---
+++ Smart_Calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float dailyUsage, savedEnergy = 0, carbonReduction;
+    char useLEDs, unplugDevices, useSmartThermostat, useSolar;
+
+    cout << fixed << setprecision(2); 
+    cout << "Welcome to the Smart Living Energy Calculator!\n";
+    cout << "Enter your average daily electricity usage in kWh: ";
+    cin >> dailyUsage;
+
+    
+}
